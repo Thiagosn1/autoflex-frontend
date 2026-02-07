f28ea3e11eb5f20103e110ef379309666b25789a
Thiagosn1 <70329966+Thiagosn1@users.noreply.github.com>
2026-02-07 14:56:33 -0300

    Add global fonts and base styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductsComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('autoflex-frontend');
